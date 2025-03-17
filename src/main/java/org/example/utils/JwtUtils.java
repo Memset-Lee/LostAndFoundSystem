dce2025a70408eff1319526ec4c3f5b3433f83e4
Memset-Lee <2697357045@qq.com>
2025-03-17 16:53:07 +0800

    The Backend Code Of LostAndFoundSystem

--- src/main/java/org/example/utils/JwtUtils.java
+++ src/main/java/org/example/utils/JwtUtils.java
@@ -3,14 +3,29 @@ package org.example.utils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.Date;
 import java.util.Map;
 
+@Component
 public class JwtUtils {
 
-    private static String signKey = "memsetXmemsetXmemsetXmemsetXmemsetXmemsetXmemset";
-    private static Long expire = 43200000L;
+    private static String signKey;
+    private static Long expire;
+
+    @Value("${jwt.signKey}")
+    private String tempSignKey;
+    @Value("${jwt.expiration}")
+    private Long tempExpire;
+
+    @PostConstruct
+    public void init() {
+        signKey = tempSignKey;
+        expire = tempExpire;
+    }
 
     //生成Jwt令牌
     public static String generateJwt(Map<String, Object> claims) {
